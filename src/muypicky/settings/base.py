0799b2aacd72eab9e22fe4b6894fdbd53d136634
pospolitaki <pospolitaki.agmail.com>
2017-12-13 23:32:02 +0200

    base and local settings had been updated

--- src/muypicky/settings/base.py
+++ src/muypicky/settings/base.py
@@ -138,4 +138,12 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
 #LOGOUT_REDIRECT_URL = '/login'
-
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
