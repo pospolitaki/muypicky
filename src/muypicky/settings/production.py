323b5d1d81edd70860d47d63aaf4253d9e0bfbcf
pospolitaki <pospolitaki.agmail.com>
2017-12-14 15:38:00 +0200

    updated production database

--- src/muypicky/settings/production.py
+++ src/muypicky/settings/production.py
@@ -89,10 +89,10 @@ WSGI_APPLICATION = 'muypicky.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 import dj_database_url
 db_from_env = dj_database_url.config()
